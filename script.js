c10350f5f090f919c800145fdc35fba60ab1db75
borawsu <borawus1@gmail.com>
2025-02-01 16:07:50 +0100

    final
    final version

--- script.js
+++ script.js
@@ -6,14 +6,35 @@ function validateEmail(email) {
     
     }
 
+    function emailReturn(){
+        document.getElementById("error-state").id = "email-input"
+        document.getElementById("error-message").style.display = "none"
+    }
+
 
 document.getElementById("newsletter-btn").onclick = function(){
     const email = document.getElementById("email-input").value
+    
 
     if (!validateEmail(email)){
         document.getElementById("error-message").style.display = "block"
-        document.getElementById("email-input").id = "test"
+        document.getElementById("email-input").id = "error-state"
+    } 
+    else {
+        document.getElementById("main-block").style.display = "none"
+        document.getElementById("main-block-success").style.display = "flex"
+        document.getElementById("confirmation-email").textContent = email
     }
+
+    if (document.getElementById("error-state")){
+        setTimeout(emailReturn, 5000);
+    }
+}
+
+
+document.getElementById("dismiss-message-btn").onclick = function(){
+    document.getElementById("main-block-success").style.display = "none"
+    document.getElementById("main-block").style.display = "flex"
 }
 
 
